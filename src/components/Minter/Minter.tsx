0150203ff062358716e2b6f09030ec6a8c1c82d5
Shun Kakinoki <shunkakinoki@gmail.com>
2021-12-11 10:06:58 -0800

    perf: ini minter ux

--- src/components/Minter/Minter.tsx
+++ src/components/Minter/Minter.tsx
@@ -8,11 +8,41 @@ import {
   useReadContract,
   useTokenBalance,
   useTokenMetadata,
+  useUserAddress,
 } from "ethereal-react";
 
 import React, { Suspense } from "react";
 
 import { useTokenId } from "@/hooks/useTokenId";
+
+export const Mint = ({
+  contract,
+  index,
+}: {
+  contract: Contract;
+  index: number;
+}) => {
+  const address = useUserAddress();
+  const tokenID = useReadContract(
+    contract,
+    "tokenOfOwnerByIndex",
+    address,
+    index,
+  );
+  const metadata = useTokenMetadata(contract, tokenID);
+
+  return (
+    <div className="mt-3">
+      <h3 className="text-3xl text-center">{metadata.name}</h3>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        className="mt-3 w-[300px] h-[300px]"
+        src={metadata.image}
+        alt={metadata.name}
+      />
+    </div>
+  );
+};
 
 export const Minted = ({
   transaction,
@@ -24,13 +54,11 @@ export const Minted = ({
   tokenId: number;
 }) => {
   const confirmation = useWaitForTransaction(transaction);
-  const tokenURI = useReadContract(contract, "tokenURI", tokenId);
   const metadata = useTokenMetadata(contract, tokenId);
+
   return (
-    <div className="flex justify-center text-2xl">
-      Minted!
-      {confirmation.status}
-      {JSON.stringify(tokenURI)}
+    <div>
+      Minted!{confirmation.status}
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         className="w-[300px] h-[300px]"
@@ -48,17 +76,26 @@ export const Minter = ({ contract }: { contract: Contract }) => {
   const balance = useTokenBalance(contract);
 
   if (balance.toNumber() !== 0) {
-    return <div>Minted already: {JSON.stringify(balance)}</div>;
+    return (
+      <div>
+        <h2 className="text-2xl text-center">Minted already:</h2>
+        {Array.from({ length: balance.toNumber() }, (_, index) => {
+          return <Mint key={index} index={index} contract={contract} />;
+        })}
+      </div>
+    );
   }
 
   if (data) {
     return (
       <Suspense fallback={<div className="my-2 text-2xl">Minting...</div>}>
-        <Minted
-          contract={contract}
-          tokenId={Number(tokenId)}
-          transaction={data}
-        />
+        <div>
+          <Minted
+            contract={contract}
+            tokenId={Number(tokenId)}
+            transaction={data}
+          />
+        </div>
       </Suspense>
     );
   }

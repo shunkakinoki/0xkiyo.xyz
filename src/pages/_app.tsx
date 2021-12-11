688e5954296ae1bec013bc3547660294ecd348d8
Shun Kakinoki <shunkakinoki@gmail.com>
2021-12-10 23:24:04 -0800

    chore: update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 
-import "tailwindcss/tailwind.css";
+import "@/styles/index.css";
 
 const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,15 @@
-module.exports = require("@shunkakinoki/tailwindcss");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["VT323", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};

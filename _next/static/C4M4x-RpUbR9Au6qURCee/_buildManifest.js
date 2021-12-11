eaff585b066dcf096a3fdfd204001ba470afe63b
shunkakinoki <shunkakinoki@users.noreply.github.com>
2021-12-11 12:07:38 +0000

    deploy: 6eb6e9bdb568e34130cb14596384fe176841c9f1

---
+++ _next/static/C4M4x-RpUbR9Au6qURCee/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1b9404e3dcd11d32.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
